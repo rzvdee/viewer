64fb9daa4466688821e4e07a50d9f651344d9a31
Daniil Veremchuk <veremchukda@email.wofford.edu>
2012-03-21 17:38:50 -0400

    index replace

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html> 
+<head>
+<meta http-equiv="Content-Type" content="text/html;charset=utf-8" >
+<link href="style.css" rel="stylesheet" type="text/css" >  
+<title>airmanga | beautiful manga</title>
+</head>
+<body>
+	<div id="header">
+		<div class="head">
+		<a href="aboutus.htm">About Us</a>
+		<h1>airmanga</h1>
+		<div class=".cleaner">&nbsp;</div>
+		</div>
+	</div>
+	<div id="wrapper">
+		<h1>Make manga beautiful.</h1>
+		<div class="motto">
+		Our goal is to deliver the latest manga and manwha in a clean and beautiful way (<a href="info.htm">Read More...</a>)
+		</div>
+		<div class="split">splitter</div>
+		<div id="container">
+			<div id="rsidebar">
+				<img src="images/latest.png" alt="latest uploads:" />
+				<ul class="mangaupdates">
+					<li><a href="view/index.php/Naruto/02">Naruto 01</a> - Flying Niggers </li>
+					<li><a href="#">Title 2</a> - updates number 2 and 3 and 4</li>
+					<li><a href="#">Title 3</a> - updates numbers to infinity</li>
+				</ul>
+			</div>
+			<div id="lsidebar">
+				<img src="images/twitter.png" alt="twitter" />
+				<ul class="twitterupdates">
+					<li>left sidebar aka twitter</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</body>
+lol
